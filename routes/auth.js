69cb077ec363bbede36ec8923c100040c2b0d702
Shailesh K <shaileshkachhara89@gmail.com>
2025-07-31 17:16:41 +0530

    changes in default user.

--- routes/auth.js
+++ routes/auth.js
@@ -61,21 +61,24 @@ router.post('/sign-in', async (req, res) => {
 
 router.post('/user_add', async (req, res) => {
   const { email, password } = req.body;
-  const role = req.body.role || 'user';
+  const role = req.body.role || 'DO';
 
   try {
-    // Check if email already exists
-    const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-    if (existingUser.rows.length > 0) {
-      return res.status(400).json({ error: 'Email already in use' });
-    }
+      // Check if email already exists
+      const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+      if (existingUser.rows.length > 0) {
+        return res.status(400).json({ error: 'Email already in use' });
+      }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await pool.query('INSERT INTO users (email, password, role) VALUES ($1, $2, $3)', [email, hashedPassword, role]);
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (err) {
-    res.status(500).json({ error: 'User registration failed' });
-  }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await pool.query('INSERT INTO users (email, password, role) VALUES ($1, $2, $3)', [email, hashedPassword, role]);
+      res.status(201).json({ message: 'User registered successfully' });
+    } 
+    catch (err) 
+    {
+      res.status(500).json({ error: 'User registration failed' });
+      console.error(err);
+    }
 });
 
 export default router;
